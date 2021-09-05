0fb63b9ab509f5d273abf7adefc83559ed08cd70
Isaev Nikita <isaev.nikitka@yandex.ru>
2021-09-05 13:02:01 +0300

    add test with flake8

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Теперь тестируем workflow!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
